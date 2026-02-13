95ec9aaaef64b16cfef2f62e40d6a65b3d77823e
rajsrivastav344 <srivastavraj344@gmail.com>
2026-02-13 21:46:36 +0530

    Your commit message

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,18 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/', // For local + Express. Change to '/Examly/' for GitHub Pages
   plugins: [react()],
-  // base: '/', // default is '/' so you can even remove this line
-  build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
-          }
-        }
-      }
-    }
-  }
 });
